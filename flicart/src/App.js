8c1df05fdee5e20676d966cd93d9e7f971f63ce1
Ashmit Yadav <beingashmit@gmail.com>
2024-06-13 16:58:47 +0530

    initiated register page

---
+++ flicart/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Do whatever you're doing to spoil yourself but atleast do something which will help you in the future </h1>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ flicart/src/component/Register.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+    const[username,setUsername]=useState("")
+    const[password,setPassword]=useState("")
+    const[confirmpassword,setConfirmPassword]=useState("")
+
+const register=()=>{
+    
+}
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
